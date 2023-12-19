8964956d1a89d041b82c4ea50d4d7bb34cf27a17
Wilyer Queipo <wilyerqueiporiera@gmail.com>
2023-12-18 21:31:16 -0400

    Bug fix

--- src/Services/fetchApi.js
+++ src/Services/fetchApi.js
@@ -1,6 +1,7 @@
 import { toast } from "sonner";
 import { rollbackUser } from "../Store/userSlice";
 
+// Simulation of an optimistic UI, when an error occurs the state is rolled back
 export function fecthToDataBase({ idUserDeleted, previusState, store }) {
 	fetch(`https://jsonplaceholder.typicode.com/users/${idUserDeleted.id}`, {
 		method: "DELETE",
@@ -11,7 +12,7 @@ export function fecthToDataBase({ idUserDeleted, previusState, store }) {
 			}
 		})
 		.catch((err) => {
-			console.log("Ha ocurrido un error:", err);
+			console.log("An error has occurred:", err);
 			toast.error(
 				`An error occurred while deleting the user: ${idUserDeleted.id}`,
 			);
